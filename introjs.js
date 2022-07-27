2dc42e87d21991fa41f823298bbe7de2702137b5
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-07-27 15:15:10 -0600

    Javascript

---
+++ introjs.js
@@ -0,0 +1,14 @@
+// Alecta en JS
+
+/*alert ("Hola Mundo");
+alert ("Hola Mundo2!");*/
+
+// CONSOLA EN JS
+/*console.log('Hola, saludamos a la consola!!');
+console.log(2+2);
+console.log('Mostrando una informacion');
+console.log('Estamos mostrando una advertencia');
+console.error('Mostrando un error');*/
+
+//Documento Write
+document.write('Hola este mensaje se mostraracen mi web =)');
